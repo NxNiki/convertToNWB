76e9c417db9bfaef540769b740e8cf02d4cc712b
Tom Donoghue <tdonoghue.research@gmail.com>
2021-10-27 10:54:16 -0400

    update docs

--- conv/__init__.py
+++ conv/__init__.py
@@ -1,4 +1,4 @@
-""""Code utilities for converting single-unit data."""
+""""Code utilities for converting single-unit data to NWB."""
 
 from conv.task import Task
 from conv.parser import process_logfile
