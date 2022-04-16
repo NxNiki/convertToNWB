7db7ae99cffa46bbaa96fd8b3c905478e6913be0
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-15 21:49:16 -0400

    udpate NBs

--- conv/__init__.py
+++ conv/__init__.py
@@ -1,5 +1,5 @@
 """"Code utilities for converting single-unit data to NWB."""
 
 from conv.task import Task
-from conv.parser import process_logfile
+from conv.parser import process_session
 from conv.io import get_files, load_config, load_configs, save_config
