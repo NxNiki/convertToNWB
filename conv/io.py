9bd59fa1524577de0ad7882e2c02a9b7f4f92acd
Tom Donoghue <tdonoghue.research@gmail.com>
2023-04-29 10:52:06 -0400

    tweak IO imports

--- conv/io.py
+++ conv/io.py
@@ -1,9 +1,9 @@
 """Utility functions for managing files."""
 
 # Link in functions from convnwb - ADD ANY EXTRA FUNCTIONS NEEDED
-from convnwb.io import (load_config, load_configs, load_task_object,
-                        save_config, save_task_object, save_nwbfile,
-                        open_h5file, save_txt)
+from convnwb.io import (load_config, load_configs, save_config,
+                        load_task_object, save_task_object,
+                        open_h5file, save_nwbfile)
 from convnwb.io.utils import get_files, make_session_name, make_file_list, missing_files
 
 ###################################################################################################
