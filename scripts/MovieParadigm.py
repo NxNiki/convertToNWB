37d81b82ee102ad093aa007a4b6d3228b7ab2c2f
NxNiki <michaelniki1988@gmail.com>
2024-03-18 11:46:20 -0700

    draft

--- scripts/MovieParadigm.py
+++ scripts/MovieParadigm.py
@@ -1,37 +1,63 @@
 """
 create files in .nwb format for the movie paradigm experiments
 """
-import glob
 from typing import List
 import os
 import re
 
-VALID_DEVICE = ['neuralynx', 'blackrock']
+VALID_DEVICE = {
+    'neurolynx': 'neuralynx',
+    'blackrock': 'blackrock'
+}
 
 
 class MovieParadigm:
 
-    def __init__(self, input_folders: List[str], project_folder, device='neuralynx'):
+    def __init__(self, input_folders: List[str], project_folder: str, device: str ='neuralynx') -> None:
         """
         Basic parameters to create pipeline for the movie paradigm experiment.
 
         Parameters:
-        input_folders (List[str]): The name of the person to greet.
+            input_folders (List[str]): The folders of movie paradigm experiments. This should be for a single patient.
+
+            project_folder (str): The directory to save the converted .nwb files.
+
 
         Returns:
-        str: A greeting message that includes the input name.
+            str: A greeting message that includes the input name.
         """
+
         self.input_folders = input_folders
         self.project_folder = project_folder
         self.device = device  # 'neuralynx' or 'blackrock'
 
-    def parse_input_folder(self):
-        if self.device == 'neuralynx':
-            patient_folders = glob.glob(rf"{self.input_folder}/D\d{3}")
-            patient_ids =
 
+    @property
+    def patient_id(self) -> str:
+        if self.device == VALID_DEVICE['neuralynx']:
+            patient_ids = [re.findall(folder, r'D\d{3}') for folder in self.input_folders]
+        elif self.device == VALID_DEVICE['blackrock']:
+            raise NotImplementedError("blackrock not implemented")
         else:
             raise ValueError(f"Unknown device: {self.device}. Valid values are {VALID_DEVICE}")
+
+        patient_id = list(set(patient_ids))
+        if len(patient_ids) == 0:
+            raise ValueError(f"")
+        return patient_id
+
+    def parse_input_folder(self) -> List[dict]:
+        """
+        Extract information about patient, experiment, and channel based on directory and file names.
+        confirm the patient id are same in the input folders.
+
+        Returns:
+            A list of dicts with fields patient, experiments, and channels. For each
+        """
+
+
+
+        return subject_info
 
     def create_folder(self, subject_id, experiment):
         pass
